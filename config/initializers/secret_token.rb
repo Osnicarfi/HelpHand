ec048695d5594b79a69fb8fac3bbc38a9c71fa58
Osni Carfi <osnicarfi@hotmail.com>
2014-05-03 23:50:04 -0300

    initial landing page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Projeto::Application.config.secret_key_base = 'b90905f53050a3933789ea83fa0c4ae938236a516bbbfaf59c192b3e2a0de59affd7c7deb91e07509e51177bfd9f6975c917a1fd59eb257e796b02c89fe032cd'
