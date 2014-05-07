73cc4270d096ba3eb223ea440d460b294bb8f94d
Osni Carfi <osnicarfi@hotmail.com>
2014-05-07 19:34:13 -0300

    mudanca visual faq add

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Projeto::Application.routes.draw do
   devise_for :users
   root "pages#home"
+  get "faq" => "pages#faq"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
