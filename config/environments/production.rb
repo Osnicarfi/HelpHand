d1035390edeae1159e5a05888e826f5cd9782363
Osni Carfi <osnicarfi@hotmail.com>
2014-05-07 00:56:51 -0300

    niver linda

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Projeto::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #required for heroku
+  #Set with the righ page
+   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
